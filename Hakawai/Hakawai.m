a1ddb44833e86249b077879980043398b0dc7be5
Austin Zheng <azheng@linkedin.com>
2014-08-18 14:23:46 -0700

    Initial Commit

---
+++ Hakawai/Hakawai.m
@@ -0,0 +1,13 @@
+//
+//  Hakawai.m
+//  Hakawai
+//
+//  Created by Austin Zheng on 8/18/14.
+//  Copyright (c) 2014 LinkedIn. All rights reserved.
+//
+
+#import "Hakawai.h"
+
+@implementation Hakawai
+
+@end
