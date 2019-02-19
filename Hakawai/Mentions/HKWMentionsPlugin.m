dc9b2a6206ad4029f9f5aad4cf69f572abe46de8
Xuhui Chen <xuchen@linkedin.com>
2019-02-19 12:24:27 -0800

    Warning cleanup: declaration shadows a local variable

--- Hakawai/Mentions/HKWMentionsPlugin.m
+++ Hakawai/Mentions/HKWMentionsPlugin.m
@@ -1899,9 +1899,9 @@ typedef NS_ENUM(NSInteger, HKWMentionsState) {
         case HKWAccessoryViewModeFreeFloating: {
             // The chooser view is attached to the top level view. Add constraints appropriately.
             __strong __auto_type parentTextView = self.parentTextView;
-            parentTextView.onAccessoryViewAttachmentBlock = ^(UIView *accessoryViewview, __unused BOOL ignored) {
+            parentTextView.onAccessoryViewAttachmentBlock = ^(UIView *accessoryView, __unused BOOL ignored) {
                 if (self.customModeAttachmentBlock) {
-                    self.customModeAttachmentBlock(accessoryViewview);
+                    self.customModeAttachmentBlock(accessoryView);
                 }
             };
             [parentTextView attachFreeFloatingAccessoryView:view absolutePosition:origin];
