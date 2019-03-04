77b5084534531f160f961ea57004107bdc0e761b
Dylan Gattey <dgattey@linkedin.com>
2019-03-04 09:47:47 -0800

    Exposes HKWRoundedRectBackgroundAttributeName via extern
    This allows us to switch back to variable comparison instead of using string equals, as this change exposes the variable externally of the library without exposing two copies of it.

--- Hakawai/Core/TextKit/HKWCustomAttributes.m
+++ Hakawai/Core/TextKit/HKWCustomAttributes.m
@@ -10,15 +10,10 @@
 //  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //
 
-#import "HKWRoundedRectBackgroundAttributeValue.h"
+#import "HKWCustomAttributes.h"
 
 /*!
- An attribute which draws text with a rounded rectangle background effect.
-
- Rounded rectangle background attributes should be paired with \c HKWRoundedRectBackgroundAttributeValue objects.
-
- \warning Don't apply this attribute to more than three lines' worth of contiguous text, as visual glitches will occur.
- I suspect this may be a problem with the layout manager methods which return bounding rects.
+ This string is exposed via OBJC_EXTERN in the header.
  */
 
-OBJC_EXTERN NSString* _Nonnull const HKWRoundedRectBackgroundAttributeName;
+NSString* _Nonnull const HKWRoundedRectBackgroundAttributeName = @"HKWRoundedRectBackgroundAttributeName";
