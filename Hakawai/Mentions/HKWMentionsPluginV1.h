af8e7f7e43cffaa9753e2e40d3ab6ca90e69ffed
bkoatz <bkoatz@linkedin.com>
2020-08-11 23:55:33 -0700

    Move instance methods to class headers
    Cannot have instance methods in protocol for conversion to swift

--- Hakawai/Mentions/HKWMentionsPluginV1.h
+++ Hakawai/Mentions/HKWMentionsPluginV1.h
@@ -33,6 +33,46 @@ typedef NS_ENUM(NSInteger, HKWMentionsState) {
 NS_ASSUME_NONNULL_BEGIN
 
 @interface HKWMentionsPluginV1 : NSObject <HKWMentionsPlugin>
+
+/*!
+ Instantiate a mentions plug-in with the specified chooser mode, no control characters, and a default search length of
+ 3 characters.
+ */
++ (nonnull instancetype)mentionsPluginWithChooserMode:(HKWMentionsChooserPositionMode)mode;
+
+/*!
+ Instantiate a mentions plug-in with the specified chooser mode, control character set, and search length.
+
+ \param controlCharacterSet    a \c NSCharacterSet containing the character or characters that should be used to begin
+                               an explicit mention, or nil if explicit mentions should not be enabled
+ \param searchLength           the number of characters to wait before beginning an implicit mention, or 0 or a negative
+                               value if implicit mentions should not be enabled
+ */
++ (nonnull instancetype)mentionsPluginWithChooserMode:(HKWMentionsChooserPositionMode)mode
+                            controlCharacters:(NSCharacterSet *_Null_unspecified)controlCharacterSet
+                                 searchLength:(NSInteger)searchLength;
+
+/*!
+ Instantiate a mentions plug-in with the specified chooser mode, control character set, search length, a color for
+ unselected mentions text, and a background color and text color for selected mentions text.
+ */
++ (nonnull instancetype)mentionsPluginWithChooserMode:(HKWMentionsChooserPositionMode)mode
+                            controlCharacters:(NSCharacterSet *_Null_unspecified)controlCharacterSet
+                                 searchLength:(NSInteger)searchLength
+                              unselectedColor:(UIColor *_Null_unspecified)unselectedColor
+                                selectedColor:(UIColor *_Null_unspecified)selectedColor
+                      selectedBackgroundColor:(UIColor *_Null_unspecified)selectedBackgroundColor;
+
+/*!
+ Instantiate a mentions plug-in with the specified chooser mode, control character set, search length, custom attributes
+ to apply to unselected mentions, and custom attributes to apply to selected mentions.
+ */
++ (nonnull instancetype)mentionsPluginWithChooserMode:(HKWMentionsChooserPositionMode)mode
+                            controlCharacters:(NSCharacterSet *_Null_unspecified)controlCharacterSet
+                                 searchLength:(NSInteger)searchLength
+                  unselectedMentionAttributes:(NSDictionary *_Null_unspecified)unselectedAttributes
+                    selectedMentionAttributes:(NSDictionary *_Null_unspecified)selectedAttributes;
+
 @end
 
 NS_ASSUME_NONNULL_END
